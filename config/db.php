3b3afbc0c0b9b88a9549daa56df772796da42f05
Byron877 <60860232+Byron877@users.noreply.github.com>
2020-10-13 18:24:26 -0600

    Conexion Base Datos
    Aca subo los cambios en los archivos los cuales trabaje, como tambien  el nombre de la base de datos , asi tambien le creacion de usuarios pra que puedan ingresar mediante el login ! ....     #Usuario: #Zay contrase;a:123456 #Emi contrase;a:123456 #Profe contrase;a:123456

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'root');//Usuario de tu base de datos
 define('DB_PASS', '');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'simple_invoice');//Nombre de la base de datos
+define('DB_NAME', 'db_facturacion');//Nombre de la base de datos
  
 /*Datos de la empresa*/
 define('NOMBRE_EMPRESA', 'SISTEMAS WEB LA');
